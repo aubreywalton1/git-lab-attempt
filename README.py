c89b0f17404c7b6d5678cf3220db5e3180484f91
aubreywalton1 <walton.aubrey@csmstudents.org>
2019-05-17 16:12:57 +0000

    ,waeriuSDhfiuod

--- README.py
+++ README.py
@@ -1,11 +1,3 @@
 # git-lab-attempt
 # Git lab
-print ("What is up, world?")
 print ("hello")
-print ("Is this working?")
-print ("bet")
-print ("yesss")
-print ("like this")
-print ("this is fun")
-print ("bet")
-print ("ok im done")
